9a9f964e6e60c6ab8903a4759055b8942df9365b
Aiki YOUGAI <phoenix22.you@gmail.com>
2017-12-04 03:17:28 +0900

    make src/Board.jsx

---
+++ src/Board.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export class Board extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Board</h2>
+      </div>
+    );
+  }
+}
--- src/Game.jsx
+++ src/Game.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import {Board} from './Board'
 
 export class Game extends React.Component {
-  render (){
-    return <h1>Stack-Connect-Four</h1>
+  render() {
+    return (
+      <div>
+        <h1>Stack-Connect-Four</h1>
+        <div className="game">
+          <Board />
+        </div>
+      </div>
+    );
   }
 }
