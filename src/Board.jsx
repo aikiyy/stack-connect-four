af90ed243b8e6d65bc9fc011c30d64bd6d3e612a
Aiki YOUGAI <phoenix22.you@gmail.com>
2017-12-04 23:48:59 +0900

    add squares in Board

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 
 export class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(props.width * props.depth).fill(1)
+    };
+  }
+
   render() {
+    var list = [];
+
+    for(var i in this.state.squares){
+      list.push(<li>{this.state.squares[i]}</li>);
+    }
+
     return (
       <div>
         <h2>Board</h2>
+        <div>{this.props.width}</div>
+        <div>{this.props.depth}</div>
+        <div>
+          <ul>
+            {list}
+          </ul>
+        </div>
       </div>
     );
   }
--- src/Game.jsx
+++ src/Game.jsx
@@ -3,11 +3,14 @@ import {Board} from './Board'
 
 export class Game extends React.Component {
   render() {
+    const depth = 10;
+    const width = 10;
+
     return (
       <div>
         <h1>Stack-Connect-Four</h1>
         <div className="game">
-          <Board />
+          <Board depth={depth} width={width} />
         </div>
       </div>
     );
