25478b945da2a5d71df1b103a30f8c2617f6c2d9
Aiki YOUGAI <phoenix22.you@gmail.com>
2017-12-06 23:42:28 +0900

    fix click -> X method bug

--- src/Board.jsx
+++ src/Board.jsx
@@ -3,31 +3,17 @@ import {Square} from './Square'
 import './index.css'
 
 export class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(props.width * props.depth).fill(null)
-    };
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
-  }
-
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />
+    return <Square num={i} value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />
   }
 
   render() {
     var list = [];
 
     for(let i = 0; i < this.props.depth; i++) {
-      list.push(<div className="board-list"></div>);
+      list.push(<div key={i} className="board-list"></div>);
       for(let j = 0; j < this.props.width; j++) {
-        let k = {i} * this.props.depth + this.props.width;
-//        list.push(<Square value={this.state.squares[k]} onClick={() => this.handleClick(k)} />);
+        const k = i * this.props.width + j;
         list.push(this.renderSquare(k))
       }
     }
--- src/Game.jsx
+++ src/Game.jsx
@@ -3,15 +3,30 @@ import {Board} from './Board'
 import './index.css'
 
 export class Game extends React.Component {
-  render() {
-    const depth = 7;
-    const width = 15;
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(props.width * props.depth).fill(null)
+    };
+  }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
+  render() {
     return (
       <div>
         <h1>Stack-Connect-Four</h1>
         <div className="game">
-          <Board depth={depth} width={width} />
+          <Board
+            depth={this.props.depth}
+            width={this.props.width}
+            squares={this.state.squares} 
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
       </div>
     );
