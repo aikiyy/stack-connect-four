741ebc4827d97d6ddb1c76707f6653150d2ecc2d
Aiki YOUGAI <phoenix22.you@gmail.com>
2017-12-07 22:52:36 +0900

    add calculateWinner function about horizontal

--- src/Game.jsx
+++ src/Game.jsx
@@ -7,22 +7,31 @@ export class Game extends React.Component {
     super(props);
     this.state = {
       squares: Array(props.width * props.depth).fill(null),
-      xIsNext: true
+      xIsNext: true,
+      winner: ''
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
     squares[i] = this.state.xIsNext ? 'X' : 'O';
+    let winner = calculateWinner(squares, i, this.props);
     this.setState({
       squares: squares,
-      xIsNext: !this.state.xIsNext
+      xIsNext: !this.state.xIsNext,
+      winner: winner
     });
   }
 
   render() {
+    const winner = this.state.winner
     let status;
-    status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    if (winner != '') {
+      status = 'Winner: ' + winner
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div>
         <h1>Stack-Connect-Four</h1>
@@ -39,5 +48,67 @@ export class Game extends React.Component {
         </div>
       </div>
     );
+  }
+}
+
+function calculateWinner(squares, i, props) {
+  let playerChar = squares[i];
+  // 横判定
+  if (checkHorizontal(squares, i, playerChar, props)) {
+    return squares[i]
+  };
+  // 縦判定
+  // 斜め判定（北西から南西）
+  // 斜め判定（北西から南西）
+  return ''
+}
+
+function isNullSquare(squares, i) {
+  if (squares[i] == null) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function checkHorizontal(squares, i, playerChar, props) {
+  let chainNum = 0;
+  // 左側チェック
+  if (i%props.width != 0) {
+    chainNum += checkLeftHorizontal(squares, i-1, playerChar, props, 0);
+  }
+  // 右側チェック
+  if (i%(props.width-1) != 0) {
+    chainNum += checkRightHorizontal(squares, i+1, playerChar, props, 0);
+  }
+  // 勝利判定
+  if (chainNum == 3) {
+    return true
+  } else {
+    return false
+  }
+}
+
+// 左側につながっている個数を再帰的にチェック
+function checkLeftHorizontal(squares, i, playerChar, props, chainNum) {
+  if (isNullSquare(squares, i) || i%props.width == 0) {
+    return chainNum
+  }
+  if (squares[i] == playerChar) {
+    return checkLeftHorizontal(squares, i-1, playerChar, props, chainNum+1);
+  } else {
+    return chainNum
+  }
+}
+
+// 右側につながっている個数を再帰的にチェック
+function checkRightHorizontal(squares, i, playerChar, props, chainNum) {
+  if (isNullSquare(squares, i) || i%(props.width-1) == 0) {
+    return chainNum
+  }
+  if (squares[i] == playerChar) {
+    return checkRightHorizontal(squares, i+1, playerChar, props, chainNum+1);
+  } else {
+    return chainNum
   }
 }
